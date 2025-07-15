768cf8f451bf14bafe952e7cf19487a8aafdd478
holdonalina <elli.born1@gmail.com>
2025-07-15 16:41:51 +0200

    Solution

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the position of Fibonacci number: ");
+int position = int.Parse(Console.ReadLine());
+
+int result = MathHelpers.GetFibonacci(position - 1);
+
+Console.WriteLine($"Fibonacci number at position {position} is {result}"); 
+
+public static class MathHelpers
+{
+  public static int GetFibonacci(int n)
+  {
+    if (n == 0)
+      return 0;
+    else if (n == 1)
+      return 1;
+    else
+      return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+  }
+}
